49a0d1d0b7c3c02558040cde52d042cea0cc8803
ornelas91 <anderson_ornelas91@hotmail.com>
2022-01-20 16:52:14 -0300

    Atualizando

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,3 +22,21 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+const skillsContent = document.getElementById('skills__content'),
+        skillsHeader = document.getElementById('.skills__header')
+
+function toggleSkills() {
+    let itemClass = this.parantNode.className
+
+    for(i = 0; i < skillsContent.clientHeight; i++) {
+        skillsContent[1].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggletSkills)   
+}) 
