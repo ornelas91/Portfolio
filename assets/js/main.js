7a68f71b1662562c623b2c0fc77dd833807f33ac
ornelas91 <anderson_ornelas91@hotmail.com>
2022-01-22 16:07:20 -0300

    Finalizando serviços

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,3 +64,24 @@ tabs.forEach(tab =>{
     })
 }) */
 
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalViews) => {
+            modalViews.classList.remove('active-modal')
+        })
+    })
+})
